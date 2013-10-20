0fc0ffc756026e87be9c0628ccfb64ff472c7d8b
Andy Stewart <lazycat.manatee@gmail.com>
2013-10-20 10:24:16 +0800

    Update.

---
+++ main.py
@@ -0,0 +1,47 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2011 ~ 2012 Deepin, Inc.
+#               2011 ~ 2012 Wang Yong
+# 
+# Author:     Wang Yong <lazycat.manatee@gmail.com>
+# Maintainer: Wang Yong <lazycat.manatee@gmail.com>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtQuick import QQuickView
+from PyQt5.QtGui import QSurfaceFormat
+from PyQt5 import QtCore
+from PyQt5.Qt import QColor
+import os
+import sys
+import signal
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    
+    view = QQuickView()
+    
+    surfaceFormat = QSurfaceFormat()
+    surfaceFormat.setAlphaBufferSize(8)
+    view.setFormat(surfaceFormat)
+    
+    view.setColor(QColor(0, 0, 0, 0))
+    view.setFlags(QtCore.Qt.FramelessWindowHint)
+    view.setSource(QtCore.QUrl.fromLocalFile(os.path.join(os.path.dirname(__file__), 'main.qml')))
+    view.show()
+    
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    sys.exit(app.exec_())
---
+++ main.qml
@@ -0,0 +1,16 @@
+import QtQuick 2.0
+
+Rectangle {
+      width: 400
+      height: 400
+	  color: Qt.rgba(0, 200, 0, 0.5)
+      radius: 3
+	  smooth: true
+	  
+	  Text {
+		  text: "hello world"
+		  anchors.horizontalCenter: parent.horizontalCenter
+		  anchors.verticalCenter: parent.verticalCenter
+		  color: "white"
+	  }
+}
