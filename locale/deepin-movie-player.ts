12afb4eb126fea2ab6dff019464b584e9fb9e966
Hualet Wang <mr.asianwang@gmail.com>
2014-06-03 10:36:52 +0800

    translation tools

---
+++ locale/deepin-movie-player.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../main.qml" line="333"/>
+        <source>Banana</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
