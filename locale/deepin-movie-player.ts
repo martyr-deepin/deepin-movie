035ab2975d2d60fb124a237e0decb4b4f04de796
Hualet Wang <mr.asianwang@gmail.com>
2014-06-10 15:06:21 +0800

    update po

--- locale/deepin-movie-player.ts
+++ locale/deepin-movie-player.ts
@@ -2,10 +2,216 @@
 <!DOCTYPE TS>
 <TS version="2.0">
 <context>
-    <name>main</name>
+    <name>AboutSection</name>
     <message>
-        <location filename="../main.qml" line="333"/>
-        <source>Banana</source>
+        <location filename="../AboutSection.qml" line="11"/>
+        <source>Deepin Movie</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AboutSection.qml" line="26"/>
+        <source>Version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AboutSection.qml" line="42"/>
+        <source>Deepin Movie is a video player designed for Linux users. It surpports a variety of video formats and features.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../AboutSection.qml" line="48"/>
+        <source>Deepin Movie is free software licensed under GNU GPLv3</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>InfomationWindow</name>
+    <message>
+        <location filename="../InfomationWindow.qml" line="28"/>
+        <source>File Type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfomationWindow.qml" line="35"/>
+        <source>File Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfomationWindow.qml" line="42"/>
+        <source>Resolution</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfomationWindow.qml" line="49"/>
+        <source>Movie Duration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfomationWindow.qml" line="56"/>
+        <source>File Path</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfomationWindow.qml" line="65"/>
+        <source>Copy To Clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../InfomationWindow.qml" line="80"/>
+        <source>Confirm</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainController</name>
+    <message>
+        <location filename="../MainController.qml" line="292"/>
+        <source>Forward To </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainController.qml" line="298"/>
+        <source>Backward To </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainController.qml" line="306"/>
+        <location filename="../MainController.qml" line="311"/>
+        <location filename="../MainController.qml" line="316"/>
+        <source>Playback Rate: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainController.qml" line="306"/>
+        <location filename="../MainController.qml" line="311"/>
+        <source>(Press %1 to restore)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainController.qml" line="321"/>
+        <location filename="../MainController.qml" line="326"/>
+        <location filename="../MainController.qml" line="334"/>
+        <location filename="../MainController.qml" line="344"/>
+        <source>Volume: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../MainController.qml" line="342"/>
+        <source>Muted</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OpenFileDialog</name>
+    <message>
+        <location filename="../OpenFileDialog.qml" line="5"/>
+        <source>Please choose one or more file</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OpenFolderDialog</name>
+    <message>
+        <location filename="../OpenFolderDialog.qml" line="5"/>
+        <source>Please choose folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PreferenceWindow</name>
+    <message>
+        <location filename="../PreferenceWindow.qml" line="86"/>
+        <source>Fit video to player</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PreferenceWindow.qml" line="92"/>
+        <source>Fit player to video</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PreferenceWindow.qml" line="101"/>
+        <source>Resize interface to last closed size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PreferenceWindow.qml" line="107"/>
+        <source>Enter fullscreen mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PreferenceWindow.qml" line="116"/>
+        <source>Clear playlist when opening new file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PreferenceWindow.qml" line="121"/>
+        <source>Resume playback after restarting player</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PreferenceWindow.qml" line="126"/>
+        <source>Continue to next video automatically</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PreferenceWindow.qml" line="312"/>
+        <source>Pause/Play</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PreferenceWindow.qml" line="312"/>
+        <location filename="../PreferenceWindow.qml" line="320"/>
+        <location filename="../PreferenceWindow.qml" line="328"/>
+        <source>Disabled</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PreferenceWindow.qml" line="320"/>
+        <source>Fullscreen</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PreferenceWindow.qml" line="328"/>
+        <source>Volume</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PreferenceWindow.qml" line="346"/>
+        <source>Font</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PreferenceWindow.qml" line="356"/>
+        <source>Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PreferenceWindow.qml" line="365"/>
+        <source>Border Width</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PreferenceWindow.qml" line="374"/>
+        <source>Position</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PreferenceWindow.qml" line="379"/>
+        <source>Bottom</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../PreferenceWindow.qml" line="380"/>
+        <source>Top</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TitleBar</name>
+    <message>
+        <location filename="../TitleBar.qml" line="59"/>
+        <source>Deepin Movie</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
