c107f0159e3de4ce741cea4c443498e85a6fcb75
Hualet Wang <mr.asianwang@gmail.com>
2014-11-19 15:43:02 +0800

    change default size
    Change-Id: Iecc20202e30f58696e5873289db013dedcdb2fc7
    tower: https://tower.im/projects/d4e0ff64158645f28d99a9ca72d0c27b/todos/d696659752234ae0b763be2f4e61d550

--- constant.py
+++ constant.py
@@ -3,20 +3,20 @@
 
 # Copyright (C) 2011 ~ 2012 Deepin, Inc.
 #               2011 ~ 2012 Wang Yong
-# 
+#
 # Author:     Wang Yong <lazycat.manatee@gmail.com>
 # Maintainer: Wang Yong <lazycat.manatee@gmail.com>
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
@@ -29,8 +29,8 @@ PROJECT_NAME = "deepin-movie"
 CONFIG_DIR = os.path.join(XDG_CONFIG_HOME, PROJECT_NAME)
 MAIN_QML = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'main.qml')
 
-DEFAULT_WIDTH = 840
-DEFAULT_HEIGHT = 560
-MINIMIZE_WIDTH = 320
-MINIMIZE_HEIGHT = 180
 WINDOW_GLOW_RADIUS = 8
+DEFAULT_WIDTH = 840 + WINDOW_GLOW_RADIUS * 2
+DEFAULT_HEIGHT = 560 + WINDOW_GLOW_RADIUS * 2
+MINIMIZE_WIDTH = 320 + WINDOW_GLOW_RADIUS * 2
+MINIMIZE_HEIGHT = 180 + WINDOW_GLOW_RADIUS * 2
