cd39340dfda7046dd7c49985a2a070b1944b779a
Hualet Wang <mr.asianwang@gmail.com>
2015-01-06 15:35:13 +0800

    forbit showing info on invalid files
    tower: https://tower.im/projects/d4e0ff64158645f28d99a9ca72d0c27b/todos/87eb984dc67a4c7aacf4b694bf41b696?proto=https
    
    Change-Id: Iad46ef10578f5e67ebcb7d45168918f6c12716b2

--- src/controllers/menu_controller.py
+++ src/controllers/menu_controller.py
@@ -456,7 +456,8 @@ class MenuController(QObject):
         self.menu.getItemById("_playlist_remove_item").isActive = url != ""
         self.menu.getItemById("_playlist_open_position").isActive = url != "" \
             and utils.urlIsNativeFile(url)
-        self.menu.getItemById("_playlist_information").isActive = url != ""
+        self.menu.getItemById("_playlist_information").isActive = url != "" \
+            and utils.urlIsNativeFile(url)
         self.menu.getItemById("_playlist_clear").isActive = not playlistEmpty
 
         self.menu.showRectMenu(QCursor.pos().x(), QCursor.pos().y())
