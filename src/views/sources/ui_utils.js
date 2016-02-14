197d817a39101836a38ad22bffad60a1b3937959
Hualet Wang <mr.asianwang@gmail.com>
2016-02-14 10:37:36 +0800

    improve copyright
    Change-Id: Id40f44805ae9663d8b115476e04f179661ac4595

--- src/views/sources/ui_utils.js
+++ src/views/sources/ui_utils.js
@@ -1,3 +1,12 @@
+/**
+ * Copyright (C) 2014 Deepin Technology Co., Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ **/
+
 function inRectCheck(point, rect) {
     return rect.x <= point.x && point.x <= rect.x + rect.width &&
     rect.y <= point.y && point.y <= rect.y + rect.height
